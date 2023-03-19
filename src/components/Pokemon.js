960868330f5e69d0490e9d5bb6ecd25d46d9bfc4
Kyle Joseph <kyle@Kyles-Air.hsd1.or.comcast.net>
2023-03-19 01:40:19 -0700

    Added links to Pokemon website

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -80,7 +80,9 @@ function Pokemon({ pokemonName, pokemonURL }) {
         <img className="pokemon-sprite" src={sprite} alt="pokemon sprite"></img>
       }
       {renderNumber()}
+        <a href={`https://www.pokemon.com/us/pokedex/${pokemonName}`} target="_blank" rel="noreferrer">
       <h4 className="pokemon-name">{pokemonName}</h4>
+        </a>
       {displayTypes()}
     </div>
   )
