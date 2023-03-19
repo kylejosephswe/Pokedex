3ed3ebc60d3941611d42b07e4df4aef547fbefb8
Kyle Joseph <kyle@Kyles-Air.hsd1.or.comcast.net>
2023-03-19 01:06:00 -0700

    Added font

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <h1 className="header-title">Pokedex</h1>
       <Pokedex />
-    </>
+    </div>
   );
 }
 
